65624e135b818ea45ea4a763492ca4aa9e777081
Matthew Anderson <anderson.matt1397@gmail.com>
2020-01-31 04:58:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(array)# code an argument here
   while i < array.length
   puts "#{i + 1}. #{array[i]}"
   i += 1
-#Your code here
+  end#Your code here
 end
 
 def summon_captain_planet# code an argument here
